f903c3d3eccd31f8ab0041f5e700dd0fb39c140e
pszczesz <pszczesz@poczta.onet.pl>
2016-09-20 09:55:45 +0200

    ini2

--- cw1/cw1.php
+++ cw1/cw1.php
@@ -8,16 +8,17 @@
         <?php 
        // phpinfo();
          $a = 12;
-         $b = 0;
+         $b = 23;
          echo '<p>'.$a.' + '.$b.' = '.($a+$b).'</p>';
          echo "<p>{$a} - {$b} = ".($a-$b)."</p>\n"; 
-         echo "<p>{$a} * {$b} = ".($a*$b)."</p>\n"; 
+         echo "<p>{$a} * {$b} = ".($a*$b)."</p>\n";
+         $wynik2 = $b!=0 ? round($a/$b,2) : "brak wyniku";
          if($b!=0){
-             $wynik = $a/$b;
+             $wynik = round($a/$b,2);
          }else{
              $wynik = "nie dzielimy przez zero";
          }
-         echo "<p>{$a} / {$b} = ".$wynik."</p>\n"; 
+         echo "<p>{$a} / {$b} = ".$wynik2."</p>\n"; 
         ?>           
     </body>
 </html>

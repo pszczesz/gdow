171f089f9076a34f058df411cfcbde4dc8dc7124
pszczesz <pszczesz@poczta.onet.pl>
2016-09-23 10:22:40 +0200

    ini2

--- cw3/cw3.php
+++ cw3/cw3.php
@@ -7,16 +7,17 @@
     </head>
     <body>
         <h1>Tabliczka mnożenia</h1>
+        <h3>Pętla for</h3>
         <table>
             <?php
-            $col = 10;
-            $row = 10;
+            $col = 4;
+            $row = 4;
             for ($i = 1; $i <= $row; $i++) {
-                $pp = $i%2===0 ? "class='add'":"";
+                $pp = $i % 2 === 0 ? "class='add'" : "";
                 echo "<tr {$pp}>\n";
                 for ($j = 1; $j <= $col; $j++) {
-                    $result = $i*$j;
-                    $first = ($i===1 || $j===1) ?
+                    $result = $i * $j;
+                    $first = ($i === 1 || $j === 1) ?
                             "class='first'" :
                             "";
                     echo "\t<td {$first}>{$result}</td>\n";
@@ -25,5 +26,21 @@
             }
             ?>
         </table>
+
+        <h3>Pętla while...</h3>
+        <p>
+            <?php
+            $suma = 0;
+            while ($suma < 40) {
+                $liczba = rand(1, 15);
+                if ($liczba % 3 === 0) {
+                    $znak = $suma === 0 ? "" : "+";
+                    echo $znak . $liczba;
+                    $suma += $liczba;
+                }
+            }
+            echo '=' . $suma;
+            ?>
+        </p>
     </body>
 </html>

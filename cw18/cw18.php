e16deb9cf5b0b8597bdaa631919baf5fc886008a
pszczesz <pszczesz@poczta.onet.pl>
2016-12-02 10:10:22 +0100

    ini2

--- cw18/cw18.php
+++ cw18/cw18.php
@@ -9,10 +9,13 @@
     <body>
         <?php
         require_once 'repository.php';
+        require_once 'functions.php';
         $dane = Repository::getAllPeople();
         foreach ($dane as $item) {
             echo $item->info();
         }
+        $students = Repository::getAllStudents();
+        echo StudentsToHtmlTab($students);
         ?>
     </body>
 </html>
--- cw18/functions.php
+++ cw18/functions.php
@@ -1,5 +1,21 @@
 <?php
-
+require_once 'uczen.php';
 function StudentsToHtmlTab(array $students){
+//    echo "<pre>";
+//    var_dump($students);
+//    echo "</pre>";
+    $html = "<table>\n";
+    $html .= "<tr><th>Lp</th><th>Imię</th><th>Nazwisko</th>"
+            . "<th>Średnia ocen</th></tr>\n";
+    $i=0;
+    foreach ($students as $student) {
+        $i++;
+        $html .= "<tr><td>{$i}</td><td>{$student->getImie()}</td>"
+            . "<td>{$student->getNazwisko()}</td> "
+            . "<td>{$student->getSrOcen()}</td></tr>\n";
+    }
+    return $html."</table>\n";
+}
+function StudentsToHtmlList(array $students){
     
 }
